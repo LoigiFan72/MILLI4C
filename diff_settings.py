6f88dc91f1312a8928ccf115e5399485110b99c2
LoigiFan72 <stephenakins855@gmail.com>
2026-02-17 21:38:33 -0500

    Remove Dev Things from CTR Server Test

--- diff_settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def apply(config, args):
-    config["baseimg"] = "tools/disasm/baserom/exefs/code.bin"
-    config["myimg"] = "build/us/code.bin"
-    config["source_directories"] = ["."]
-    config["arch"] = "armel"
-    config["arch_objdump"] = "arm"
-    config["objdump_executable"] = os.environ.get('DEVKITARM') + "/bin/arm-none-eabi-objdump"
